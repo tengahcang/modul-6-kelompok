09eaca088a4994736392f5986d0ea78484aef233
tengahcang <cangg522@gmail.com>
2023-06-20 00:20:22 +0700

    ini upadate tadi ada yang kurang

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\EmployeeController;
 
 /*
 |--------------------------------------------------------------------------
